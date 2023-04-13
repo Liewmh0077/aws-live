b59dfc7432fc6e384fdd5112877ba06a6bbf79a3
Liewmh0077 <67110818+Liewmh0077@users.noreply.github.com>
2023-04-13 12:44:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.cmoy300s7jni.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liewminhui-employee"
+customregion = "us-east-1"
