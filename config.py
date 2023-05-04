936a37628b2d31298914c07c4899160cb981911a
Liewmh0077 <67110818+Liewmh0077@users.noreply.github.com>
2023-05-04 17:35:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-2.cmoy300s7jni.us-east-1.rds.amazonaws.com"
+customhost = "employee.cmoy300s7jni.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
